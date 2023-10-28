35c187a8d61ee6ec8b82725a7790a58fa3208f4a
Bogdan Anczarski <bogdan@SuperKomputer.L440>
2023-10-28 10:31:58 +0200

    działająca wersja

--- 01_guess_numb/guess_the_number.py
+++ 01_guess_numb/guess_the_number.py
@@ -1,8 +1,21 @@
 from random import randint
 
+def guess_the_number():
+    rand_numb = randint(1, 100)
+    answer = None
+    while answer != rand_numb:
+        try:
+            answer = int(input("Guess the number: "))
+        except ValueError:
+            answer = int(input("This is not number. Try again: "))
 
+        if answer < rand_numb:
+            print("Too small")
+        elif answer > rand_numb:
+            print('Too big')
+        else:
+            return 'You win'
 
-try:
-    answer = int(input("Guess the number: "))
-except:
-    answer = int(input("This is not number. Try again: "))
+    # return f'wylosowana liczba to: {rand_numb}, a wybrana {answer}'
+
+print(guess_the_number())
