3daed4f709c56da1b2193679f3ea8b04816ae475
Bogdan Anczarski <bogdan@SuperKomputer.L440>
2023-11-03 14:57:09 +0100

    działająca wersja

--- 04_guess_numb_2_flask/choose_numb.py
+++ 04_guess_numb_2_flask/choose_numb.py
@@ -4,15 +4,16 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def guess_number():
-    if request.method == "GET:":
-        return render_template("form.html", min=0, max=1000)
+    if request.method == "GET":
+        return render_template("form.html", min='0', max='1000')
     else:
         form = request.form
-        min = int(form.get("min"))
-        max = int(form.get("max"))
+        min = int(form["min"])
+        max = int(form["max"])
+        # answer = form["answer"]
+        # guess = form["guess", 500] # guess = (max - min) // 2 + min zamienic 500 na wzor jesli pobierze inne zmienne
         answer = form.get("answer")
-        guess = int(form.get("guess", 500)) # guess = (max - min) // 2 + minzamienic 500 na wzor jesli pobierze inne zmienne
-
+        guess = int(form.get("guess", (max - min) // 2 + min))
 
         if answer == "You won":
             return render_template("win.html", guess=guess)
@@ -21,6 +22,8 @@ def guess_number():
 
         elif answer == "Too big":
             max = guess
+
+        guess = (max - min) // 2 + min
         return render_template("choose.html", guess=guess, min=min, max=max)
 
 
