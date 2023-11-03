f37b461a9caaecac779b45a047283f73b9d5ffb1
Bogdan Anczarski <bogdan@SuperKomputer.L440>
2023-11-03 21:30:18 +0100

    If command is wrong else is not stable

--- 06_2001/2001_mod1.py
+++ 06_2001/2001_mod1.py
@@ -1,4 +1,4 @@
-from random import randint
+from random import randint, choice
 
 def dices_throw(first_dice = 'random', second_dice = 'random'):
     dices = {'D3': randint(1, 3), 'D4': randint(1, 4),
@@ -8,7 +8,8 @@ def dices_throw(first_dice = 'random', second_dice = 'random'):
     if first_dice in dices and second_dice in dices:
         return dices[first_dice] + dices[second_dice]
     elif first_dice == 'random' or second_dice == 'random':
-        pass
+        dices_list = ['D3', 'D4', 'D6', 'D8', 'D10', 'D12', 'D20', 'D100']
+        return dices[choice(dices_list)] + dices[choice(dices_list)]
     else:
         print('Wrong dices. Try again')
         first_dice = input('Choose first dice ("D3, D4, D6, D8, D10, D12, D20, D100")\n')
@@ -26,7 +27,7 @@ def sum_points(player_points, throw_points):
 
 first_dice = input('Choose first dice ("D3, D4, D6, D8, D10, D12, D20, D100")\n')
 second_dice = input('Choose second dice ("D3, D4, D6, D8, D10, D12, D20, D100")\n')
-input('press enter to throw dices.\n')
+
 human_player = dices_throw(first_dice, second_dice)
 print('You have', human_player, ' points.')
 computer_player = dices_throw(first_dice, second_dice)
@@ -35,7 +36,7 @@ print('Computer have', computer_player, 'points.')
 while human_player < 2001 and computer_player < 2001:
     first_dice = input('Choose first dice ("D3, D4, D6, D8, D10, D12, D20, D100")\n')
     second_dice = input('Choose second dice ("D3, D4, D6, D8, D10, D12, D20, D100")\n')
-    input('\npress enter to throw dices.\n')
+
     human_player = sum_points(human_player, dices_throw(first_dice, second_dice))
     print('You have', human_player, ' points.')
     computer_player = sum_points(computer_player, dices_throw(first_dice, second_dice))
